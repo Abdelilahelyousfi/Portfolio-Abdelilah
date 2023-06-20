c3447a07e3aa5ee27ea87f4241af360659f3c051
Abdelilah <abdelilah.elyousfi12@gmail.com>
2023-06-20 15:20:38 +0200

    Your commit message

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+// La fonction `updateList()` met à jour la liste des titres en fonction de leur position sur la page.
 function updateList() {
-	const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
-		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
-	});
+    // Sélectionne tous les éléments h1 et h2 de la page et les trie en fonction de leur position verticale.
+    const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+        return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+    });
 
-	document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
-	
-	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+    // Supprime la classe "selected-circle" de tous les éléments ayant la classe "selected-circle".
+    document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+
+    // Ajoute la classe "selected-circle" à l'élément .nav-dot correspondant au premier titre de la liste.
+    document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
 }
 
+// Appelle la fonction `updateList()` une fois au chargement initial de la page pour mettre à jour les titres visibles.
 updateList();
+
+// Ajoute un écouteur d'événement pour mettre à jour les titres visibles à chaque fois que l'utilisateur fait défiler la page.
 window.addEventListener('scroll', () => {
     updateList();
-})
+});
