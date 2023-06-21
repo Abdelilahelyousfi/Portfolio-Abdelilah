4a910cf8720ef94e70a2451aeb90ebcc5556d798
Abdelilah <abdelilah.elyousfi12@gmail.com>
2023-06-21 13:08:48 +0200

    Your commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,6 @@ module.exports = {
         'badge': '#3F3F51',
         'input-border': '#565666',
         'input': '#2A2A35'
-        'input': '#2A2A35'
-
       },
       fontFamily: {
         'poppins': ["'Poppins'", 'sans-serif']
